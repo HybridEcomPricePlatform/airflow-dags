721c54f9935fca5449a04ec040f9ff3fe833cd3d
Salma2134 <aitsioui.salma@etu.uae.ac.ma>
2026-06-12 23:27:29 +0200

    remove greatExpectationsFolder

--- great_expectations/datasources/raw_datasource.py
+++
@@ -1,9 +0,0 @@
-# datasources/raw_datasource.py
-import great_expectations as gx
-
-def get_raw_datasource(context):
-    datasource = context.sources.add_or_update_pandas(
-        name="price_events_datasource"
-    )
-    asset = datasource.add_dataframe_asset(name="price_events_asset")
-    return asset
